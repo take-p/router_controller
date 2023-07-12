017dd614a123ea5b3c060b16dae303a79d0ca85f
take-p <merry3nosheep@gmail.com>
2023-07-13 01:43:38 +0900

    feat:ipアドレスも環境変数を参照するように変更

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.by import By
 import os
 import sys
 
+ip = os.getenv("ROUTER_IP_ADDRESS")
 username = os.getenv("ROUTER_USERNAME")
 password = os.getenv("ROUTER_PASSWORD")
-url = "http://192.168.3.2"
 
 # ユーザー名とパスワードをURLに埋め込む
-auth_url = f"http://{username}:{password}@192.168.3.2"
+auth_url = f"http://{username}:{password}@{ip}"
 
 driver = webdriver.Firefox()
 driver.get(auth_url + "/cgi-bin/luci/content/net_filtering/url_filter?nocache")
@@ -29,9 +29,8 @@ if len(sys.argv) > 1:
         checkbox.click()
         driver.execute_script("goToApply()") # 設定を反映
         print("フィルタリングをonにしました")
-
     # 引数が 'off' で、チェックボックスが選択されている場合、クリックしてチェックを外す
-    if sys.argv[1] == 'off' and checkbox.is_selected():
+    elif sys.argv[1] == 'off' and checkbox.is_selected():
         checkbox.click()
         driver.execute_script("goToApply()") # 設定を反映
         print("フィルタリングをoffにしました")
