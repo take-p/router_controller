4bcb8d4759fd6695263d528ec4c89097a3d09de8
take-p <merry3nosheep@gmail.com>
2023-08-05 00:34:43 +0900

    fix:バグ修正

--- main2.py
+++ main2.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import schedule
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+import datetime
 
 def task(arg):
     ip = os.getenv("ROUTER_IP_ADDRESS")
@@ -14,7 +16,7 @@ def task(arg):
     auth_url = f"http://{username}:{password}@{ip}"
 
     options = Options()
-    options.headless = True
+    options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.get(auth_url + "/cgi-bin/luci/content/net_filtering/url_filter?nocache")
 
@@ -22,12 +24,15 @@ def task(arg):
     checkbox = driver.find_element(By.ID, "enable_url_filter")
     
     # チェックボックスが既にチェックされているかどうかを確認
-    if not checkbox.is_selected():
+    #if not checkbox.is_selected():
         # チェックボックスが選択されていない場合、クリックしてチェックをつける
-        checkbox.click()
+    #    checkbox.click()
 
         # 設定を反映
-        driver.execute_script("goToApply()")
+    #    driver.execute_script("goToApply()")
+
+    dt_now = datetime.datetime.now()
+    print(dt_now.strftime('%Y年%m月%d日 %H:%M:%S'), end=" ")
 
     if arg == 0 and checkbox.is_selected():
         checkbox.click()
@@ -37,10 +42,15 @@ def task(arg):
         checkbox.click()
         driver.execute_script("goToApply()") # 設定を反映
         print("フィルタリングON")
+    else:
+        print("ルーター設定の変更に失敗しました。")
 
 # スケジュール登録
-schedule.every().days.at("9:00").do(task, 1)
+schedule.every().days.at("09:00").do(task, 1)
 schedule.every().days.at("18:00").do(task, 0)
+#schedule.every().days.at("00:24").do(task, 1)
+
+print("自動フィルタリング制御プログラム起動")
 
 while True:
     schedule.run_pending()
